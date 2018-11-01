fac3d57316a336772ecfef09b28b4a0a50a0dada
onegin 🚀 <evgeniyivanets@gmail.com>
2018-11-01 22:53:00 +0200

    1.0.0

---
+++ README.md
@@ -0,0 +1,80 @@
+# PHP Console Logger that handles his outputs to stdout/stderr according to LogLevel
+
+> Designed as simple as possible, following [php-fig](https://www.php-fig.org) PSR's and [codestyle](https://en.wikipedia.org/wiki/Programming_style) tools
+
+### Tiny and _simple-first_ **PHP Logger** library that output data to console's `stdout`/`stderr` in depends of `LogLevel`.
+### Implements interfaces defined by [PSR-3](https://www.php-fig.org/psr/psr-3/).
+
+## Install
+
+```sh
+composer require micronext/php-std-logger
+```
+
+## Development
+
+Clone this repo
+
+```sh
+git clone https://github.com/micronext/php-std-logger
+cd php-std-logger
+composer install
+```
+
+## Usage:
+
+```php
+<?php
+
+namespace YourAmazingApp;
+
+use Psr\Log\LogLevel;
+use MicroNext\Console\Logger;
+
+$logger = new Logger;
+
+$logger->emergency("emergency test");
+$logger->alert("alert test");
+$logger->critical("critical test");
+$logger->error("error test");
+$logger->warning("warning test");
+$logger->notice("notice test");
+$logger->info("info test");
+$logger->debug("debug test");
+$logger->log(null, "log test");
+$logger->log(LogLevel::NOTICE, "notice throught log test";
+```
+
+## Currently `std output` separated to 2 targets:
+
+> [LogLevel definition @ PSR-3](https://www.php-fig.org/psr/psr-3/#5-psrlogloglevel)
+
+```c
+stderr << log
+when "LogLevel"
+     ::EMERGENCY // System is unusable.
+     ::ALERT // Action must be taken immediately.
+     ::CRITICAL  // Critical conditions.
+     ::ERROR // Runtime errors that do not require immediate action but should typically be logged and monitored.
+
+stdout << log
+when LogLevel
+     ::WARNING // Exceptional occurrences that are not errors.
+     ::NOTICE // Normal but significant events.
+     ::INFO // Interesting events.
+     ::DEBUG // Detailed debug information.
+```
+
+## MIT LICENSE
+
+```
+Copyright 2018 (c) MicroNext
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
+
+[2018 © MicroNext](https://github.com/micronext)
---
+++ src/Logger.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace MicroNext\Console;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+/**
+ * Class `StdOut\Logger`.
+ *
+ * @api
+ *
+ * @author Yevhenii Ivanets <evgeniyivanets@gmail.com>
+ * Outputs logs to `std::out` or `std::err`
+ */
+class Logger extends AbstractLogger implements LoggerInterface
+{
+    /**
+     * @api
+     *
+     * @var string Template
+     */
+    public const OUTPUT_FORMAT = " {date}\t| [{level}]: {message}{lf} {date}\t| [{level}]: {context}{lf}---------------------{lf}";
+
+    /**
+     * ### Array that defines associations, where we gonna write `$message`, `std::out` or `std::err`
+     * ```
+     *   std::out << WARNING | NOTICE | INFO | DEBUG
+     *   std::err << EMERGENCY | CRITICAL | CRITICAL | ERROR
+     * ```
+     * #### IMPORTANT!
+     * It's `private` for reason.
+     * - User DO NOT need to know split logic realization
+     * - According to `Psr\Log\LoggerInterface` and `Psr\Log\AbstractLogger`,
+     * we MUST to implement a `log` method to DESCRIBE behaviour, but NOT change user interaction
+     * - Since PHP 7.1.0 visibility modifiers are allowed for class constants.
+     *
+     * @property array(Psr\Log\LogLevel=>std::void) OUT
+     *
+     * @since 7.1.0
+     * @see http://php.net/manual/en/language.oop5.constants.php#language.oop5.basic.class.this
+     */
+    private const OUT = [
+        // Error logs
+        LogLevel::EMERGENCY => STDERR,
+        LogLevel::CRITICAL => STDERR,
+        LogLevel::CRITICAL => STDERR,
+        LogLevel::ERROR => STDERR,
+        // Logs
+        LogLevel::WARNING => STDOUT,
+        LogLevel::NOTICE => STDOUT,
+        LogLevel::INFO => STDOUT,
+        LogLevel::DEBUG => STDOUT,
+    ];
+
+    /**
+     * ## Logs with an arbitrary level.
+     * Writes `$message` to corresponding output depends on `$logLevel`
+     * If it's described at `StdLogger::OUT` outputs there, else into std:error.
+     *
+     * @api
+     *
+     * @param mixed  $level   if empty, will use `LogLevel::DEBUG` fallback
+     * @param string $message
+     * @param array  $context
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $level = $level ?? LogLevel::DEBUG;
+        $output = (array_key_exists($level, self::OUT)) ? self::OUT[$level] : STDERR;
+        $data = '';
+        if (!empty($context)) {
+            try {
+                $json = json_encode($context, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT | JSON_UNESCAPED_SLASHES);
+                $data .= $json;
+            } catch (\Exception $e) {
+                $data = '[X] Error occured when tried to convert additional context to JSON.'.PHP_EOL.'\t'.$e->getMessage();
+            }
+        }
+        fwrite($output, $this->format($level, $message, $data));
+    }
+
+    private function format(string $level, string $message, string $context): string
+    {
+        return trim(strtr(self::OUTPUT_FORMAT, [
+            '{date}' => $this->getDate(),
+            '{level}' => $level,
+            '{message}' => $message,
+            '{context}' => $context,
+            '{lf}' => PHP_EOL,
+        ]));
+    }
+}
